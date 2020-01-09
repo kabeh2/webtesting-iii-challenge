94f0bd1969745a79028a6cca24068f994c18d4b1
kabeh2 <mattcnk@hotmail.com>
2020-01-09 18:44:36 -0500

    Updates

---
+++ precache-manifest.f8238c9a41d97f68fe5e40a04e79739c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2e093230d68704e3434dd2754a97942",
+    "url": "/webtesting-iii-challenge/index.html"
+  },
+  {
+    "revision": "8810f03251180c562334",
+    "url": "/webtesting-iii-challenge/static/css/main.ed1c7ff7.chunk.css"
+  },
+  {
+    "revision": "b3766afa106c3b5a8b31",
+    "url": "/webtesting-iii-challenge/static/js/2.ef954bb7.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/webtesting-iii-challenge/static/js/2.ef954bb7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8810f03251180c562334",
+    "url": "/webtesting-iii-challenge/static/js/main.1e233d84.chunk.js"
+  },
+  {
+    "revision": "7a58bca59138afd11696",
+    "url": "/webtesting-iii-challenge/static/js/runtime-main.9c1dc7ac.js"
+  }
+]);
